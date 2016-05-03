8ab0ac69671acfaad8e0151abd9e61a772019f12
Jiao Lin <jiao.lin@gmail.com>
2016-05-02 20:43:39 -0400

    Refs #1. added files from mcvine and resources repos for arcs powder workflow

---
+++ mcvine_workflow/__init__.py
@@ -0,0 +1,12 @@
+# -*- Python -*-
+#
+# Jiao Lin <jiao.lin@gmail.com>
+#
+
+import os
+
+# root directory of mcvine workflow
+root = os.path.dirname(os.path.dirname(__file__))
+root = os.path.abspath(root)
+
+# End of file 
---
+++ mcvine_workflow/cli/__init__.py
@@ -0,0 +1,14 @@
+# -*- Python -*-
+#
+# Jiao Lin <jiao.lin@gmail.com>
+#
+
+from mcvine.cli import mcvine
+
+@mcvine.group()
+def workflow():
+    return
+
+from . import powder
+
+# End of file 
