83679a995a1bad4e502578ca6d4e33af5485b490
Jiao Lin <linjiao@ornl.gov>
2016-06-03 12:09:10 -0400

    Refs #3. coords_transform: added methods hkl2Q,Q2hkl,rtzE2hkl,hklE2rtz

--- mcvine_workflow/singlextal/coords_transform.py
+++ mcvine_workflow/singlextal/coords_transform.py
@@ -3,4 +3,60 @@
 # Jiao Lin <jiao.lin@gmail.com>
 #
 
+"""
+hkl2Q and Q2hkl:
+* hkl: miller indices
+* Q: cartesian in instrument coordinate system
+     in which z is vertical and x is along beam
+
+rtzE:
+* r, theta, z: cylindrical coordinate system. z vertical. x along beam
+  - r,z: meters
+  - theta: radian
+* for DGS, Ei is needed for computing Ef
+
+"""
+
+import numpy as np
+from mcni.utils import conversion as conv
+
+def hkl2Q(hkl, xtalori):
+    """hkl: hkl vectors (N,3)
+    """
+    m = xtalori.hkl2cartesian_mat()
+    return np.dot(hkl, m)
+
+def Q2hkl(Q, xtalori):
+    """Q: Q vectors (N,3)
+    """
+    m = xtalori.cartesian2hkl_mat()
+    return np.dot(Q, m)
+
+def rtzE2hkl(r, theta, z, E, xtalori, Ei):
+    """convert from cylinderical coordinate sytem to hkl
+    """
+    kf_dir = [r*np.cos(theta), r*np.sin(theta), z]
+    kf_dir /= np.linalg.norm(kf_dir)
+    Ef = Ei - E
+    kf_scalar = (conv.SE2V*conv.V2K) * Ef**.5
+    kf = kf_dir * kf_scalar
+    ki_scalar = conv.e2k(Ei)
+    ki = [ki_scalar, 0, 0]
+    Q = ki - kf
+    return Q2hkl(Q, xtalori)
+
+def hklE2rtz(hkl, E, xtalori, Ei, r):
+    """convert from hkl to cylinderical coordinate sytem
+    """
+    ki_scalar = conv.e2k(Ei)
+    ki = [ki_scalar, 0, 0]
+    Q = hkl2Q(hkl, xtalori)
+    kf = ki - Q
+    kf_dir = kf/np.linalg.norm(kf)
+    scale_factor = r/np.linalg.norm(kf_dir[:, :3])
+    position = kf_dir * scale_factor
+    np.allclose(np.linalg.norm(position[:, :3]), r)
+    theta = np.arctan2(position[:, 1], position[:, 0])
+    return r, theta, position[:, 2]
+
 # End of file 
---
+++ tests/mcvine_workflow/singlextal/test_coords_transform.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+#
+# Jiao Lin <jiao.lin@gmail.com>
+
+import mcvine.cli
+from mcvine_workflow import singlextal
+from mcvine_workflow.singlextal import coords_transform as ct
+from mcvine_workflow.singlextal import solve_psi
+
+import numpy as np
+
+Ei = 100
+Etarget = 35
+xtalori = singlextal.loadXtalOriConfig("Si-xtalori.yaml")
+hkl = [-6,0,0]
+psi_min, psi_max = -5., 90.
+# set crystal orientation to perfectly measure the given hkl
+solutions = solve_psi.solve(xtalori, Ei, hkl, Etarget, psi_min, psi_max)
+assert len(solutions)==1
+sol = solutions[0]
+xtalori.psi = sol
+
+def test_hkl2Q():
+    hkls = [hkl]
+    Qs = ct.hkl2Q(hkls, xtalori)
+    # print Qs
+    hkls2 = ct.Q2hkl(Qs, xtalori)
+    np.allclose(hkls, hkls2)
+    return
+
+def test_hklE2rtz():
+    hkls = [hkl]
+    r,theta,z = ct.hklE2rtz(hkls, Etarget, xtalori, Ei, r=3)
+    # print r, theta, z
+    E = np.array([Etarget])
+    hkls2 = ct.rtzE2hkl(r, theta, z, E, xtalori, Ei)
+    np.allclose(hkls, hkls2)
+    return
+
+def main():
+    test_hkl2Q()
+    test_hklE2rtz()
+    return
+
+
+if __name__ == '__main__': main()
+
+# End of file 
