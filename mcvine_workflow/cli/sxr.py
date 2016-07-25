13721c7ff098caf532ea561b53afa80d2b4869d2
Jiao Lin <linjiao@ornl.gov>
2016-07-25 18:26:43 -0400

    Refs #4. adding interface and implementations of single angle and batch reduction. next step: connecting interface and impl.

--- mcvine_workflow/cli/sxr.py
+++ mcvine_workflow/cli/sxr.py
@@ -14,7 +14,14 @@ from . import workflow
 @workflow.command()
 @click.option("--instrument-type", default='DGS', type=click.Choice(['DGS']))
 @click.option("--type", default="batch", type=click.Choice(['batch', 'single']))
-def reduce():
+@click.option("--eiguess", default=100.)
+@click.option("--eaxis", default=(0.,100.,1.), nargs=3, type=float)
+@click.option("--psi-axis", default=(-10., 120., 1.), nargs=3, type=float)
+@click.option("--psi", default=0.)
+@click.option("--eventnxs")
+@click.option("--out")
+def reduce(instrument_type, type, eiguess, eaxis, psi_axis, psi, eventnxs, out):
+    print (instrument_type, type, eiguess, eaxis, psi_axis, psi, eventnxs, out)
     return
 
 # End of file 
---
+++ mcvine_workflow/singlextal/reduction.py
@@ -0,0 +1,64 @@
+# -*- Python -*-
+#
+# Jiao Lin <jiao.lin@gmail.com>
+#
+
+def reduceScan(psis, nxs_template, outfn_template, eiguess, eaxis):
+    """run reductions on all given directories
+    
+    - psis: psi angles in degrees 
+    - nxs_template: template for input nxs path
+    - outfn_template: template for output file path
+    - eiguess: Ei in meV
+    - eaxis: Emin, Emax, dE
+    """
+    for psi in psis:
+        nxsfile = nxs_template % (psi,)
+        outfile = outfn_template % (psi,)
+        reduceOneKeepingEvents(nxsfile, psi, eiguess, eaxis, outfile)
+    return
+
+
+def reduceOneKeepingEvents(nxsfile, angle, eiguess, eaxis, outfile):
+    """reduce nxs from one angle of a single crystal scan, keeping events
+    (only do tof->E conversion)
+
+    nxsfile: input path
+    angle: psi in degrees
+    eiguess: Ei in meV
+    eaxis: Emin, Emax, dE
+    outfile: output path
+    """
+    from mantid.simpleapi import DgsReduction, SofQW3, SaveNexus, SaveNXSPE, LoadInstrument, Load, MoveInstrumentComponent, AddSampleLog
+
+    print "* working on ", nxsfile
+    # load workspace from input nexus file
+    workspace = Load(nxsfile)
+
+    # workspace name have to be unique
+    import os
+    unique_name = os.path.dirname(nxsfile).split('/')[-1]
+    wsname = 'reduced-%s' % unique_name
+
+    # keep events (need to then run RebinToWorkspace and ConvertToDistribution)
+    Emin, Emax, dE = eaxis
+    eaxis = '%s,%s,%s' % (Emin,dE, Emax)
+    DgsReduction(
+        SampleInputWorkspace = workspace,
+        IncidentEnergyGuess=eiguess,
+        UseIncidentEnergyGuess=False,
+        OutputWorkspace=wsname,
+        SofPhiEIsDistribution='0',
+        EnergyTransferRange = eaxis,
+        )
+
+    AddSampleLog(Workspace=wsname,LogName="psi",LogText=angle,LogType="Number")
+
+    SaveNexus(
+        InputWorkspace=wsname,
+        Filename = outfile,
+        Title = 'reduced',
+        )
+    return
+
+# End of file 
