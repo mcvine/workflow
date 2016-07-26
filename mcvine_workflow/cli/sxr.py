e3de811c920db1d6e8e4ee0730a0291c56cba5f6
Jiao Lin <linjiao@ornl.gov>
2016-07-26 10:28:34 -0400

    Refs #4. new commands slice2hist and getslice

--- mcvine_workflow/cli/sxr.py
+++ mcvine_workflow/cli/sxr.py
@@ -11,7 +11,11 @@ import mantid, os, stat, click, numpy as np
 
 from . import workflow
 
-@workflow.command()
+@workflow.group()
+def sxr():
+    return
+
+@sxr.command()
 @click.option("--instrument-type", default='DGS', type=click.Choice(['DGS']))
 @click.option("--type", default="batch", type=click.Choice(['batch', 'single']))
 @click.option("--eiguess", default=100.)
@@ -30,5 +34,77 @@ def reduce(instrument_type, type, eiguess, eaxis, psi_axis, psi, eventnxs, out):
     elif type == 'batch':
         reduction.reduceScan(psi_axis, eventnxs, out, eiguess, eaxis)
     return
+
+
+scan_yml_example = """
+angles: -90,90.1,3.0
+filename_pattern: work_%(angle)s/reduced_%(angle)s.nxs
+lattice: 2., 2.5, 3., 90, 90, 90
+orientation:
+ u: 1, 0, 2
+ v: 1,0,0
+"""
+
+slice_yml_example = """
+Eaxis:
+ min: 0
+ max: 90
+ N: 181
+Q_projections:
+ U:
+  proj: 1,0,0
+  proj_name: H,0,0
+  min: -5
+  max: 5
+  N: 251
+ V:
+  proj: 0,1,0
+  proj_name: 0,K,0
+  min: -1
+  max: 1
+  N: 1
+ W:
+  proj: 0,0,1
+  proj_name: 0,0,L
+  min: -1
+  max: 1
+  N: 1
+"""
+
+@sxr.command()
+@click.option("--scan", default='scan.yml')
+@click.option("--slice", default='slice.yml')
+@click.option("--out", default='out.nxs')
+def slice(scan, slice, out):
+    from mcvine.cli.config import loadYmlConfig
+    # load scan
+    scan = loadYmlConfig(scan)
+    angles = np.arange(*eval(scan.angles))
+    filenames = [
+        scan.filename_pattern % dict(angle=angle)
+        for angle in angles
+    ]
+    print angles[0], filenames[0]
+    lattice_params = eval(scan.lattice)
+    orientation = scan.orientation
+    # load slice
+    slice = loadYmlConfig(slice)
+    Eaxis = slice.Eaxis
+    Qproj_axes = slice.Q_projections
+    out = out.encode()
+    from mcvine_workflow.singlextal import reduction
+    reduction.getslice(
+        angles, filenames, lattice_params, orientation, Eaxis, Qproj_axes, out)
+    return
+
+
+@sxr.command(help="Convert mantid-create slice nxs file to histogram h5 file")
+@click.argument("mantid")
+@click.argument("histogram")
+def slice2hist(mantid, histogram):
+    mantid = mantid.encode()
+    histogram = histogram.encode()
+    from mcvine_workflow.singlextal import reduction
+    reduction.slice2hist(mantid, histogram)
 
 # End of file 
--- mcvine_workflow/singlextal/reduction.py
+++ mcvine_workflow/singlextal/reduction.py
@@ -5,6 +5,128 @@
 
 import numpy as np
 
+def slice2hist(ifile, ofile):
+    import histogram as H, histogram.hdf as hh
+    from mantid import simpleapi as msa
+
+    def eliminateUnitDimension(shape):
+        for d in shape:
+            if d>1: yield d
+        return
+
+    ws = msa.Load(ifile)
+    I = ws.getSignalArray()
+    I.shape = tuple(eliminateUnitDimension(I.shape))
+    E2 = ws.getErrorSquaredArray()
+    E2.shape = I.shape
+    axes = []
+    for i in range(ws.getNumDims()):
+        dim = ws.getDimension(i)
+        if dim.getNBins() > 1:
+            axis = H.axis(
+                dim.getName(), 
+                unit="1",
+                centers = [dim.getX(ind) for ind in range(dim.getNBins())]
+                )
+            axes.append(axis)
+        continue
+    h = H.histogram("slice", axes, data=I, errors=E2)
+    hh.dump(h, ofile)
+    return
+
+def getslice(angles, filenames,
+             lattice_params, orientation,
+             Eaxis, Qproj_axes,
+             output):
+    a,b,c,alpha,beta,gamma = lattice_params
+    u,v = orientation.u, orientation.v
+
+    from mantid.simpleapi import Load, CompressEvents, DeleteWorkspace, mtd,\
+        SetGoniometer, SetUB, CropWorkspace, ConvertToMD, MDNormDirectSC,\
+        CloneMDWorkspace, RemoveWorkspaceHistory, SmoothMD, SaveMD \
+        
+    WG= Load(','.join(filenames))
+
+    # compress events
+    CompressEvents(InputWorkspace=WG,OutputWorkspace='WG_compressed',Tolerance=0.05)
+    # clean up
+    DeleteWorkspace(WG)
+    # obtain the compressed ws
+    WG_compressed=mtd['WG_compressed']
+    WGO = WG_compressed
+
+    # geometry
+    # goniometer psi: rotate around vertical axis. counter-clock wise (ccw)
+    SetGoniometer(WGO, Axis0='psi,0,1,0,1')
+    # UB: ws, a, b, c, alpha, beta, gamma, u, v
+    # SetUB(WGO,7.255,5.002,5.548,90,96.75,90,u=[-0.333333,0,0.333333],v=[0,2,0])
+    # SetUB(WGO,a, a, a, 90, 90, 90, u=[-1.,1.,-1.],v=[2., 1., -1.])
+    SetUB(WGO,a, b, c, alpha, beta, gamma, u=u,v=v)
+    # align the energy axis?
+    CropWorkspace(
+        InputWorkspace=WGO,OutputWorkspace='WGO_cropped',
+        XMin=Eaxis.min,XMax=Eaxis.max)
+    WGO_cropped=mtd['WGO_cropped']
+    # clean up
+    DeleteWorkspace(WGO)
+
+
+    # min, max values of hklE
+    U,V,W = Qproj_axes.U,Qproj_axes.V,Qproj_axes.W
+    minn="%s,%s,%s,%s" % (U.min,V.min,W.min,Eaxis.min)
+    maxx="%s,%s,%s,%s" % (U.max,V.max,W.max,Eaxis.max)
+
+    for i in range(WGO_cropped.getNumberOfEntries()):
+        # convert events to Q vector
+        ConvertToMD(
+            InputWorkspace=WGO_cropped[i],
+            OutputWorkspace='md_i_use',
+            dEAnalysisMode="Direct",
+            QDimensions='Q3D',
+            Q3DFrames="HKL",
+            QConversionScales='HKL',
+            MinValues=minn,
+            MaxValues=maxx,
+            Uproj=U.proj,
+            Vproj=V.proj,
+            Wproj=W.proj)
+
+        # compute data and normalization for the slices at the requested axes
+        dims = dict(
+            # AlignedDim0='[-H,H,-H],-6.0,6.0, 481',
+            AlignedDim0 = '[%s],%s,%s,%s' % (U.proj_name, U.min, U.max, U.N),
+            AlignedDim1='DeltaE,%s,%s, %s' % (Eaxis.min, Eaxis.max, Eaxis.N),
+            # ,-5.45,-5.15,1',
+            AlignedDim2='[%s],%s,%s,%s' % (V.proj_name, V.min, V.max, V.N),
+            AlignedDim3='[%s],%s,%s,%s' % (W.proj_name, W.min, W.max, W.N),
+            )
+        print dims
+        a2,b2=MDNormDirectSC('md_i_use', **dims)
+
+        #
+        print 'rotation angle #%s: %s' % (i, angles[i])
+
+        # merge data
+        if i==0:
+            dataMD2=CloneMDWorkspace(a2)
+            normMD2=CloneMDWorkspace(b2)
+        else:
+            dataMD2+=a2
+            normMD2+=b2
+        continue
+
+    # clean up
+    RemoveWorkspaceHistory(dataMD2); RemoveWorkspaceHistory(normMD2)
+
+    # smooth data
+    DataSmooth2=SmoothMD(InputWorkspace=dataMD2, WidthVector=3, Function='Hat',InputNormalizationWorkspace=normMD2)
+    NormSmooth2=SmoothMD(InputWorkspace=normMD2, WidthVector=3, Function='Hat',InputNormalizationWorkspace=normMD2)
+    SmoothedSlice=DataSmooth2/NormSmooth2
+
+    SaveMD(InputWorkspace='SmoothedSlice',Filename=output)
+    return
+
+
 def reduceScan(psi_axis, nxs_template, outfn_template, eiguess, eaxis):
     """run reductions on all given directories
     
