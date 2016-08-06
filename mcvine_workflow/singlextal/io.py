031c592eedc72825a411f27546755dcc5fbeffdc
Jiao Lin <linjiao@ornl.gov>
2016-08-06 14:37:35 -0400

    Refs #6. io: added method loadXtalOriFromSampleYml

--- mcvine_workflow/singlextal/io.py
+++ mcvine_workflow/singlextal/io.py
@@ -6,6 +6,8 @@
 import numpy as np
 
 def loadXtalOriConfig(path):
+    import warnings
+    warnings.warn("This method is obsolete. Use loadXtalOriFromSampleYml")
     from mcvine.cli import config
     xtalori = config.loadYmlConfig(path)
     l = xtalori.lattice
@@ -13,15 +15,28 @@ def loadXtalOriConfig(path):
         key = 'a%d' %i
         setattr(l, key, eval(getattr(l, key)))
         continue
-    v = np.dot(l.a1, np.cross(l.a2, l.a3))
-    b1 = 2*np.pi*np.cross(l.a2, l.a3)/v
-    b2 = 2*np.pi*np.cross(l.a3, l.a1)/v
-    b3 = 2*np.pi*np.cross(l.a1, l.a2)/v
+    from .scaffolding.utils import reciprocal_basis
+    b1,b2,b3 = reciprocal_basis([l.a1, l.a2, l.a3])
+    #
     xo = xtalori.orientation
     for key in ['u','v']:
         setattr(xo, key, eval(getattr(xo, key)))
         continue
     from .XtalOrientation import XtalOrientation
     return XtalOrientation(b1,b2,b3, xo.u, xo.v, xo.psi)
+
+def loadXtalOriFromSampleYml(path):
+    from mcvine.cli import config
+    sample = config.loadYmlConfig(path)
+    bv = map(eval, sample.lattice.basis_vectors)
+    from .scaffolding.utils import reciprocal_basis
+    b1,b2,b3 = reciprocal_basis(bv)
+    #
+    xo = sample.orientation
+    for key in ['u','v']:
+        setattr(xo, key, eval(getattr(xo, key)))
+        continue
+    from .XtalOrientation import XtalOrientation
+    return XtalOrientation(b1,b2,b3, xo.u, xo.v, 0)
 
 # End of file 
