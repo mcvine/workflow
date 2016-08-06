8dca7e0df3596e6a5c8c955d55802499d96b696f
Jiao Lin <linjiao@ornl.gov>
2016-08-06 14:38:41 -0400

    Refs #6. removed cmd "mkxoyml". added cmd "solve_psi"

--- mcvine_workflow/cli/sxu.py
+++ mcvine_workflow/cli/sxu.py
@@ -49,46 +49,21 @@ orientation:
 
 @sxu.command()
 @click.argument("sample")
-@click.argument("out")
-def mkxoyml(sample, out):
-    """Create xtalori.yml file
-This cmd needs a simplified sample yaml file. Here is an example:
-
-\b
---- !example.yml
-name: sample
-lattice: 
- constants: 8.87, 8.87, 5.2, 90, 90, 90
- basis_vecotrs:
-  - 8.87, 0, 0
-  - 0, 8.87, 0
-  - 0, 0, 5.2
-orientation:
- u: 1, 0, 0
- v: 0, 1, 0
-"""
-    from mcvine.cli.config import loadYmlConfig
-    sample = loadYmlConfig(sample)
-    # parse inputs
-    bv = map(eval, sample.lattice.basis_vectors)
-    from ..singlextal.scaffolding.utils import reciprocal_basis
-    rb = reciprocal_basis(bv)
-    b1, b2, b3 = rb
-    b1 = "%s,%s,%s" % tuple(b1)
-    b2 = "%s,%s,%s" % tuple(b2)
-    b3 = "%s,%s,%s" % tuple(b3)
-    so=sample.orientation
-    u,v = so.u, so.v
-    template = """
-b1: %(b1)s
-b2: %(b2)s
-b3: %(b3)s
-u: %(u)s
-v: %(v)s
-psi: 0
-"""
-    content = template % locals()
-    open(out, 'wt').write(content)
+@click.option("--Ei", default=100.)
+@click.option("--hkl", default=(1.,0.,0.))
+@click.option("--E", default=50.)
+@click.option("--psimin", default=-90.)
+@click.option("--psimax", default=90.)
+@click.option("--number-segments", default=10)
+def solve_psi(sample, ei, hkl, e, psimin, psimax, number_segments):
+    from ..singlextal.io import loadXtalOriFromSampleYml
+    xtalori = loadXtalOriFromSampleYml(sample)
+    from ..singlextal.solve_psi import solve
+    results = solve(
+        xtalori, ei, hkl, e, psimin, psimax,
+        Nsegments = number_segments)
+    for r in results:
+        print r
     return
 
 # End of file 
--- tests/DGS/ARCS/sxu_TestCase.py
+++ tests/DGS/ARCS/sxu_TestCase.py
@@ -11,10 +11,11 @@ class TestCase(unittest.TestCase):
         self.assertEqual(o.strip(), "0.0,1.0,0.0,0.0,0.0,1.0,1.0,0.0,0.0")
         return
 
-    def test_mkxoyml(self):
-        "mcvine workflow sxu mkxoyml"
-        cmd = "mcvine workflow sxu mkxoyml KVO.yml KVO-xtalori.yml"
+    def test_solve_psi(self):
+        "mcvine workflow sxu solve_psi"
+        cmd = "mcvine workflow sxu solve_psi Si.yml --Ei=100 --hkl -5.33333 -2.66667 2.66667 --E 40 --psimin -5. --psimax 90."
         o = sp.check_output(cmd.split())
+        self.assertAlmostEqual(float(o), 46.37473, places=4)
         return
 
 if __name__ == '__main__': unittest.main()
