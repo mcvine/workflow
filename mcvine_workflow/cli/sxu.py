b0e5748c83413da1ed092742fd50005d932f7607
Jiao Lin <linjiao@ornl.gov>
2016-08-06 13:37:14 -0400

    Refs #6. added cmd "workflow sxu mkxoyml"

--- mcvine_workflow/cli/sxu.py
+++ mcvine_workflow/cli/sxu.py
@@ -46,4 +46,49 @@ orientation:
     print utils.computeOrientationStr(lattice_basis=bv, uv=uv)
     return
 
+
+@sxu.command()
+@click.argument("sample")
+@click.argument("out")
+def mkxoyml(sample, out):
+    """Create xtalori.yml file
+This cmd needs a simplified sample yaml file. Here is an example:
+
+\b
+--- !example.yml
+name: sample
+lattice: 
+ constants: 8.87, 8.87, 5.2, 90, 90, 90
+ basis_vecotrs:
+  - 8.87, 0, 0
+  - 0, 8.87, 0
+  - 0, 0, 5.2
+orientation:
+ u: 1, 0, 0
+ v: 0, 1, 0
+"""
+    from mcvine.cli.config import loadYmlConfig
+    sample = loadYmlConfig(sample)
+    # parse inputs
+    bv = map(eval, sample.lattice.basis_vectors)
+    from ..singlextal.scaffolding.utils import reciprocal_basis
+    rb = reciprocal_basis(bv)
+    b1, b2, b3 = rb
+    b1 = "%s,%s,%s" % tuple(b1)
+    b2 = "%s,%s,%s" % tuple(b2)
+    b3 = "%s,%s,%s" % tuple(b3)
+    so=sample.orientation
+    u,v = so.u, so.v
+    template = """
+b1: %(b1)s
+b2: %(b2)s
+b3: %(b3)s
+u: %(u)s
+v: %(v)s
+psi: 0
+"""
+    content = template % locals()
+    open(out, 'wt').write(content)
+    return
+
 # End of file 
--- tests/DGS/ARCS/sxu_TestCase.py
+++ tests/DGS/ARCS/sxu_TestCase.py
@@ -4,11 +4,17 @@ import unittest, os, sys, shutil, subprocess as sp
 
 class TestCase(unittest.TestCase):
     
-    def test(self):
-        "mcvine workflow sxu"
+    def test_kernelorientation(self):
+        "mcvine workflow sxu kernelorientation"
         cmd = "mcvine workflow sxu kernelorientation KVO.yml"
         o = sp.check_output(cmd.split())
         self.assertEqual(o.strip(), "0.0,1.0,0.0,0.0,0.0,1.0,1.0,0.0,0.0")
+        return
+
+    def test_mkxoyml(self):
+        "mcvine workflow sxu mkxoyml"
+        cmd = "mcvine workflow sxu mkxoyml KVO.yml KVO-xtalori.yml"
+        o = sp.check_output(cmd.split())
         return
 
 if __name__ == '__main__': unittest.main()
