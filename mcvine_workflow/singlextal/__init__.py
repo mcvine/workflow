0e0e6cc092a8922874e20abec5f84065fd4e7ae3
Jiao Lin <linjiao@ornl.gov>
2017-04-06 20:31:48 -0400

    mcvine_workflow.singlextal: move simple common data structures here

--- mcvine_workflow/singlextal/__init__.py
+++ mcvine_workflow/singlextal/__init__.py
@@ -18,6 +18,37 @@ Misc
 * Draw scan lines
 """
 
+class instrument:
+    def __init__(self, name, detsys_radius, L_m2s, offset_sample2beam, L_m2fc=None):
+        self.name = name
+        self.detsys_radius = detsys_radius
+        self.L_m2s = L_m2s
+        self.offset_sample2beam = offset_sample2beam
+        self.L_m2fc = L_m2fc
+
+class pixel:
+    def __init__(self, radius, height, pressure, position=None):
+        self.radius = radius
+        self.height = height
+        self.pressure = pressure
+        self.position = position
+
+class axis:
+    def __init__(self, min, max, step):
+        self.min, self.max, self.step = min, max, step
+
+    def ticks(self):
+        import numpy as np
+        return np.arange(self.min, self.max, self.step)
+
+class dynamics:
+
+    def __init__(self, E, q, hkl0, hkl_dir):
+        self.E = E
+        self.q = q
+        self.hkl0 = hkl0
+        self.hkl_dir = hkl_dir
+
 from .io import loadXtalOriConfig
 
 # End of file 
