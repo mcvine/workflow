d73c6ccd113be5f8ede189a90ba0d24b39d3676d
Jiao Lin <linjiao@ornl.gov>
2018-04-19 15:04:35 -0400

    Refs mcvine/ui#19. added mcvine.workflow.sample.dgs_setEi

---
+++ tests/mcvine_workflow/sample_TestCase.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+#
+# Jiao Lin <jiao.lin@gmail.com>
+
+import mcvine.cli
+from mcvine.workflow import sample
+from mcni.utils import conversion as C
+
+import numpy as np, os
+here = os.path.dirname(__file__)
+
+
+def test():
+    class powdersqe:
+        type = "powderSQE"
+        SQEhist = "Al-iqe.h5"
+        Erange = "-50*meV,50*meV"
+        Qrange = "1/angstrom, 10/angstrom"
+
+    class mysample:
+        class lattice:
+            constants = 1., 1., 1., 90., 90., 90.
+            basis_vectors = [[1,0,0],[0,1,0],[0,0,1]]
+        chemical_formula="V"
+        class shape:
+            class sphere:
+                radius = 3
+    mysample.excitations = [powdersqe]
+    Ei = 30.; ki = C.e2k(Ei)
+    sample.dgs_setEi(mysample, Ei)
+    Qmin, Qmax = sample._to_float_tuple(mysample.excitations[0].Qrange, '1./angstrom')
+    assert np.isclose(Qmin, 1.)
+    assert np.isclose(Qmax, 2*ki)
+    Emin, Emax = sample._to_float_tuple(mysample.excitations[0].Erange, 'meV')
+    assert np.isclose(Emin, -Ei)
+    assert np.isclose(Emax, Ei*.99)
+    return
+
+
+def main():
+    test()
+    return
+
+
+if __name__ == '__main__': main()
+
+# End of file 
--- workflow/sample.py
+++ workflow/sample.py
@@ -28,3 +28,54 @@ def loadSampleYml(path):
     if hasattr(sample, 'excitation'):
         del sample.excitation
     return sample
+
+
+def dgs_setEi(sample, Ei):
+    """set incident energy for DGS experiment
+    
+A sample has kernels with parameters that may be affected by Ei.
+For example, a powder SQE kernel has Qrange and Erange. 
+When this sample is put into a beam in a DGS instrument,
+it is better to simulate only within the dynamical range determined
+by Ei and instrument geometry.
+Here we do some quick calculation to estimate the dynamical range
+for the given Ei, without considering the instrument geometry.
+The main purpose of this is to limit the simulation dynamical
+range to speed up the simulation
+
+    Ei: float. unit: meV
+    """
+    from mcni.utils import conversion
+    ki = conversion.e2k(Ei) # \AA^-1
+    Qrange = (0., 2*ki)
+    Erange = (-Ei, Ei*.99)
+    for excitation in sample.excitations:
+        if 'Qrange' in excitation.__dict__:
+            excitation.Qrange = ','.join(_to_str_tuple_with_unit(
+                _combine_range(Qrange, _to_float_tuple(excitation.Qrange, '1./angstrom')),
+                '1./angstrom'))
+        if 'Erange' in excitation.__dict__:
+            excitation.Erange = ','.join(_to_str_tuple_with_unit(
+                _combine_range(Erange, _to_float_tuple(excitation.Erange, 'meV')),
+                'meV'))
+        continue
+    return
+
+
+def _combine_range(r1, r2):
+    min1, max1 = r1
+    min2, max2 = r2
+    return max(min1, min2), min(max1, max2)
+
+from mcni.units import parser
+parser = parser()
+def _to_float_tuple(s, unit):
+    values = map(parser.parse, s.split(','))
+    u = parser.parse(unit)
+    try:
+        sum(values, u)
+    except:
+        raise ValueError("The units for should be %s. Got %s" % (unit, s))
+    return tuple(v/u for v in values)
+def _to_str_tuple_with_unit(floats, unit):
+    return tuple('%s * %s' % (f, unit) for f in floats)
