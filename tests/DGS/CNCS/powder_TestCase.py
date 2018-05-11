2bb48642f0fdd28410fe991714302a308ed57eea
Jiao <linjiao@ornl.gov>
2018-05-10 20:04:18 -0400

    Update powder_TestCase.py
    larger ncount so there is less chance no neutrons reached sample

--- tests/DGS/CNCS/powder_TestCase.py
+++ tests/DGS/CNCS/powder_TestCase.py
@@ -11,7 +11,7 @@ class TestCase(unittest.TestCase):
         if os.system(cmd): raise RuntimeError("%s failed" % cmd)
         wfdir = "mcvine-workflow-powder-CNCS-V"
         assert os.path.exists(wfdir) and os.path.isdir(wfdir)
-        cmd = 'cd %s/beam; mcvine instruments cncs beam  --ncount=4e6 --nodes=2' % wfdir
+        cmd = 'cd %s/beam; mcvine instruments cncs beam  --ncount=1e7 --nodes=2' % wfdir
         if os.system(cmd):
             raise RuntimeError("%s failed" % cmd)
         cmd = 'cd %s; make NCOUNT=2e6 NODES=2' % wfdir
