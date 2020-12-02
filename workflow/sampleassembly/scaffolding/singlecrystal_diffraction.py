fc337516351ea8fe405c197c6d63683acb620dda
Jiao Lin <jiao.lin@gmail.com>
2020-12-01 19:07:43 -0800

    scaffolding for single crystal diffraction kernel

---
+++ workflow/sampleassembly/scaffolding/singlecrystal_diffraction.py
@@ -0,0 +1,33 @@
+# -*- Python -*-
+#
+# Jiao Lin <jiao.lin@gmail.com>
+#
+
+"""
+Tools for creating single crystal diffraction sample
+"""
+
+import os, numpy as np
+
+def createKernel(excitation, h2Q, orientation):
+    """
+    excitation:
+      Dd_over_d="1.e-4" lau_path="Al.lau" mosaic="5./60*deg"
+    """
+    d = dict(excitation.__dict__)
+    d.update(orientation=orientation)
+    return kernel_template % d
+
+
+kernel_template = """
+      <SingleCrystalDiffractionKernel
+        weight="1."
+        orientation="%(orientation)s"
+        Dd_over_d="%(Dd_over_d)s"
+        lau_path="%(lau_path)s"
+        mosaic="%(mosaic)s"
+      >
+      </SingleCrystalDiffractionKernel>
+"""
+
+# End of file 
