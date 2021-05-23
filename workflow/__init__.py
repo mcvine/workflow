deb136964200fdc77b62faa7f1d6b1bea951a054
Jiao Lin <jiao.lin@gmail.com>
2021-05-23 05:33:31 -0700

    added __version__ str:   workflow/__init__.py

--- workflow/__init__.py
+++ workflow/__init__.py
@@ -3,10 +3,12 @@
 # Jiao Lin <jiao.lin@gmail.com>
 #
 
+from ._version import __version__
+
 import os
 
 # root directory of mcvine workflow
 root = os.path.dirname(os.path.dirname(__file__))
 root = os.path.abspath(root)
 
-# End of file 
+# End of file
